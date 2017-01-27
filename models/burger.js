c65a8f088c7f7a5f5bccfd21b9848c41b6b254e0
Jeffrey Bauer <Jeffrey@Jeffreys-Air.attlocal.net>
2017-01-27 14:43:19 -0500

    setup

---
+++ models/burger.js
@@ -0,0 +1,18 @@
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("burgers", {
+
+      burger_name: {
+          type: Sequelize.STRING,
+          allowNull: false
+  },
+      devoured: {
+          type: Sequelize.BOOLEAN,
+          allowNull: false
+  },
+      date: {
+           type: Sequelize.STRING,
+           isDate: True,
+           allowNull: false
+  }
+  });
+  };
